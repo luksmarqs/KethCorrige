089d0898c83483def528b757f5a46415ef903c4c
luksmarqs <heylucasmg@gmail.com>
2024-02-03 00:41:07 -0300

    Segundo commit

--- src/components/Firstsection.js
+++ src/components/Firstsection.js
@@ -1,8 +1,9 @@
 import Lapis from "../assets/lapis.svg"
+import Seta from "../assets/setaCurvada.svg"
 import Image from "next/image"
 export function Firstsection() {
     return (
-        <div class="w-full h-[900px] bg-lightBlue">
+        <section class="w-full h-[900px] bg-lightBlue">
             <div class="w-full flex flex pt-[32px] pl-[32px]">
                 <div class="basis-1/5">
                     <button class="flex flex-row">
@@ -11,7 +12,7 @@ export function Firstsection() {
                     </button>
                 
                 </div>
-                <div class= "flex flex-row basis-4/5 justify-center pt-[6px] pb-[7px] pr-[25%]">
+                <div class= "flex flex-row basis-4/5 justify-center pt-[6px] pb-[7px] pr-[20%]">
                     <div class="flex gap-8">
                         <button>
                             <span class="text-lg">Início</span>
@@ -34,6 +35,30 @@ export function Firstsection() {
                     </div> 
                 </div>    
             </div>
-        </div>
+            <div class="flex flex-col">
+                <div class="flex flex-col mt-24">
+                    <span class="text-[72px] font-bold text-center">GARANTA SUA APROVAÇÃO</span>
+                    <span class="text-[72px] font-bold text-center mt-[-20px]">mande a sua redação!</span>
+                    <div class="flex justify-center mt-[15px]">
+                        <span class="text-[18px] font-medium text-center w-[526px] h-[44px]">Se você precisa de ajuda na redação não hesite, entre em contato e comece a evoluir.</span>
+                    </div>
+            </div>
+                <div class= "flex flex-row mt-[30px]">
+                    <div class="flex basis-[55.60259%] justify-end ">
+                        <button class="w-[180px] h-[57px] bg-gold rounded-full shadow-2xl shadow-gold ">
+                            <span>Comprar</span>
+                        </button>
+                    </div>
+                    <div class="flex flex-col basis-[44.39745] justify-start">
+                        <div class="flex flex-col ">
+                            <div class="rotate-[18deg]">
+                                <span class="ml-[30px] font-medium">Peça aqui!</span>
+                            </div>
+                            <Image class="h-[55px] w-[55px] ml-[20px] rotate-[5deg]"src={Seta}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
